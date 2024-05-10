f72709f68869abfb12a3a513a345daf33b9c834c
JD Nickell <jd@jdnickell.com>
2024-05-09 22:07:51 -0500

    work in progress

--- src/LatestFilingsEventPublisher/Function.cs
+++ src/LatestFilingsEventPublisher/Function.cs
@@ -16,15 +16,14 @@ namespace LatestFilingsEventPublisher;
 public class Function
 {
     // TODO: Get from configuration or ParameterStore.
-    public const string SNS_TOPIC_ARN = "YourSnsTopicArn";
-    private const string RSS_HTTP_CLIENT_NAME = "SecRssClient";
+    public const string SNS_TOPIC_ARN = "YourSnsTopicArn"; private const string RSS_HTTP_CLIENT_NAME = "SecRssClient";
+    internal int MAXIMUM_EVENTS_TO_PUBLISH_PER_INVOCATION = 1000;
+    internal const int EVENTS_TO_FETCH_PER_REQUEST = 100;
 
     private readonly IHttpClientFactory _httpClientFactory;
-
     private readonly ILogger<Function> _logger;
-
     private readonly IAmazonSimpleNotificationService _snsClient;
-
+    private readonly ILambdaRunTimestampManager _lambdaRunTimestampManager;
     private readonly XmlSerializer _xmlSerializer;
 
     /// <summary>
@@ -51,34 +50,28 @@ public class Function
     {
         serviceCollection ??= [];
 
-        // Logging
         serviceCollection.AddLogging(builder =>
         {
             builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
             builder.AddConsole();
         });
 
-        // HttpClient
         serviceCollection.AddHttpClient(RSS_HTTP_CLIENT_NAME, client =>
         {
             client.DefaultRequestHeaders.Host = "www.sec.gov";
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (None jdnickell@gmail.com)");
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (YourCompany yourcompany@example.org)");
         })
         .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
         {
             AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
         });
-        // TODO: Add retry policy.
 
-        // SNS
         serviceCollection.TryAddSingleton<IAmazonSimpleNotificationService>(serviceProvider =>
         {
-            // Optionally register Xray tracing
             return new AmazonSimpleNotificationServiceClient(new AmazonSimpleNotificationServiceConfig { RegionEndpoint = Amazon.RegionEndpoint.USEast1 });
         });
 
-
-        // XML Serializer
+        serviceCollection.TryAddSingleton<ILambdaRunTimestampManager, LastRunTimestampManager>();
         serviceCollection.TryAddSingleton(new XmlSerializer(typeof(Feed)));
 
         ServiceProvider = serviceCollection.BuildServiceProvider();
@@ -86,6 +79,7 @@ public class Function
         _httpClientFactory = ServiceProvider.GetRequiredService<IHttpClientFactory>();
         _logger = logger ?? ServiceProvider.GetRequiredService<ILogger<Function>>();
         _snsClient = ServiceProvider.GetRequiredService<IAmazonSimpleNotificationService>();
+        _lambdaRunTimestampManager = ServiceProvider.GetRequiredService<ILambdaRunTimestampManager>();
         _xmlSerializer = ServiceProvider.GetRequiredService<XmlSerializer>();
     }
 
@@ -98,35 +92,72 @@ public class Function
     {
         using (_logger.BeginScope("{AwsRequestId}", context.AwsRequestId))
         {
-            // TODO: Last run logic - Compare to a stored 'last run time' to avoid publishing events new filings that have already been published, return if there are no new filings.
-            string url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom";
-
-            var httpClient = _httpClientFactory.CreateClient(RSS_HTTP_CLIENT_NAME);
-
-            // TODO: Needs to page result.
-            HttpResponseMessage response = await httpClient.GetAsync(url);
-
-            response.EnsureSuccessStatusCode();
-
-            string result = await response.Content.ReadAsStringAsync();
-
-            using TextReader reader = new StringReader(result);
-
-            if (_xmlSerializer.Deserialize(reader) is not Feed deserializedFeed || deserializedFeed.Entries == null || deserializedFeed.Entries.Length == 0)
-                return;
-
-            // TODO: Last run logic - Store the last run time to avoid publishing events new filings that have already been published.
-            var feedUpdated = deserializedFeed.Updated;
-            _logger.LogInformation("Feed last updated: {updated}. Publishing {Count} new filings.", feedUpdated, deserializedFeed.Entries.Length);
-
-            foreach (var entry in deserializedFeed.Entries)
+            // The rate limit is 10 requests per second which is more than enough to keep up with the feed as long as this Lambda is running frequently.
+            // You could add a delay here to avoid hitting the rate limit, but you would probably be better off using a different strategy to crawl the SEC files.
+            if (MAXIMUM_EVENTS_TO_PUBLISH_PER_INVOCATION / EVENTS_TO_FETCH_PER_REQUEST > 10)
             {
-                await _snsClient.PublishAsync(new PublishRequest
-                {
-                    TopicArn = SNS_TOPIC_ARN,
-                    Message = JsonSerializer.Serialize(entry)
-                });
+                throw new InvalidOperationException("The SEC endpoint rate limit is 10 requests per second. The maximum number of events to publish per invocation may be too high.");
             }
+
+            string baseUrl = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start={0}&count={1}&output=atom";
+            var httpClient = _httpClientFactory.CreateClient(RSS_HTTP_CLIENT_NAME);
+            var lastRunTimestamp = await _lambdaRunTimestampManager.GetLastRunTimestampEasternAsync();
+            _logger.LogInformation("Last run timestamp: {LastRunTimestamp}", lastRunTimestamp);
+
+            _logger.LogInformation("Setting last run timestamp converting utc now to eastern, {Now}.", DateTime.UtcNow);
+            await _lambdaRunTimestampManager.SetLastRunTimestampAsync(DateTime.UtcNow);
+
+            var entriesPublished = 0;
+
+            // There are two ways to iterate through the feed: from 0 to maximum, or by paging in reverse order.
+            // Paging in reverse order is the safest approach because you can update the last run timestamp and be sure that you won't publish duplicates in the event of a lambda failure,
+            // but this means making a minimum of (MAXIMUM_EVENTS_TO_PUBLISH_PER_INVOCATION / EVENTS_TO_FETCH_PER_REQUEST) per invocation since we can only filter the result.
+            // Paging from 0 to maximum is more efficient because you can stop as soon as you reach the last run timestamp,
+            // but in the event of a failure you will either publish duplicates or miss entries.
+            for (int start = 0; start < MAXIMUM_EVENTS_TO_PUBLISH_PER_INVOCATION; start += EVENTS_TO_FETCH_PER_REQUEST)
+            {
+                string url = string.Format(baseUrl, start, EVENTS_TO_FETCH_PER_REQUEST);
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string result = await response.Content.ReadAsStringAsync();
+                using TextReader reader = new StringReader(result);
+
+                if (_xmlSerializer.Deserialize(reader) is not Feed deserializedFeed || deserializedFeed.Entries == null || deserializedFeed.Entries.Length == 0)
+                {
+                    break;
+                }
+
+                // The entries' updated timestamps are deserialized to local.
+                var entries = deserializedFeed.Entries.Where(x => x.Updated > lastRunTimestamp);
+                if (!entries.Any())
+                {
+                    break;
+                }
+
+                entriesPublished += entries.Count();
+
+                foreach (var entry in entries)
+                {
+                    await _snsClient.PublishAsync(new PublishRequest
+                    {
+                        TopicArn = SNS_TOPIC_ARN,
+                        Message = JsonSerializer.Serialize(entry)
+                    });
+                }
+
+                // If we get less than the requested count, we have reached the end of the feed.
+                if (entries.Count() < EVENTS_TO_FETCH_PER_REQUEST)
+                {
+                    break;
+                }
+            }
+
+            // A lambda function error is not one that can be handled in a try/catch. If an error occurs, the function may be retried.
+            // Depending on the use case consider setting this at the beginning of processing, or modifying the iterator logic to account for this.
+            await _lambdaRunTimestampManager.SetLastRunTimestampAsync(DateTime.UtcNow);
+
+            _logger.LogInformation("Finished processing feed and published {EntriesPublished} entries.", entriesPublished);
         }
     }
 }
---
+++ src/LatestFilingsEventPublisher/LastRunTimestampManager.cs
@@ -0,0 +1,87 @@
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace LatestFilingsEventPublisher;
+
+/// <inheritdoc />
+public class LastRunTimestampManager : ILambdaRunTimestampManager
+{
+    const string PARAMETER_NAME = "LatestFilingsEventPublisher-LastRunTimestamp";
+
+    /// <inheritdoc />
+    public async Task<DateTime?> GetLastRunTimestampEasternAsync()
+    {
+        using var client = new AmazonSimpleSystemsManagementClient();
+        var request = new GetParameterRequest
+        {
+            Name = PARAMETER_NAME
+        };
+        var response = await client.GetParameterAsync(request);
+
+        if (response.Parameter == null || response.Parameter.Value == null)
+        {
+            return null;
+        }
+
+        var lastRunTimeStampUtc = DateTime.Parse(response.Parameter.Value, CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal);
+
+        return GetEasternTime(lastRunTimeStampUtc);
+    }
+
+    /// <inheritdoc />
+    public async Task SetLastRunTimestampAsync(DateTime timestamp)
+    {
+        using var client = new AmazonSimpleSystemsManagementClient();
+        var request = new PutParameterRequest
+        {
+            Name = PARAMETER_NAME,
+            Value = GetEasternTime(timestamp).ToString("o"),
+            Type = ParameterType.String,
+            Overwrite = true
+        };
+        var response = await client.PutParameterAsync(request);
+        if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception("Failed to set last run timestamp.");
+        }
+    }
+
+    /// <summary>
+    /// SEC datetimes are in Eastern time. The AWS Lambda .NET 8 runtime is built on the Amazon Linux 2023 (AL2023) minimal container image.
+    /// Converts the UTC time to Eastern time for Windows or Linux depending on the host.
+    /// </summary>
+    /// <returns></returns>
+    private DateTime GetEasternTime(DateTime dateTime)
+    {
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        var timeZoneId = isWindows ? "Eastern Standard Time" : "America/New_York";
+
+        TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, easternZone); // automatically takes into account daylight saving time.
+        return easternTime;
+    }
+}
+
+/// <summary>
+/// Manager for the last run timestamp of the Lambda function getting and setting the value in Parameter Store.
+/// This is just a quick and easy option if you don't have a database or other storage mechanism available.
+/// </summary>
+public interface ILambdaRunTimestampManager
+{
+    /// <summary>
+    /// Gets the last run timestamp of the Lambda function from Parameter Store and converts it to eastern time.
+    /// </summary>
+    /// <returns>DateTime of the last run timestamp in eastern.</returns>
+    /// <exception cref="Exception"></exception>
+    Task<DateTime?> GetLastRunTimestampEasternAsync();
+
+    /// <summary>
+    /// Sets the last run timestamp (in utc) of the Lambda function in Parameter Store.
+    /// </summary>
+    /// <param name="timestamp"></param>
+    /// <returns>An awaitable Task.</returns>
+    /// <exception cref="Exception"></exception>
+    Task SetLastRunTimestampAsync(DateTime timestamp);
+}
