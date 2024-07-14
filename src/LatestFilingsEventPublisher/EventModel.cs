9f594866a4f9453fa9c04c0f65d5d1723c39b2d1
JD Nickell <jd@jdnickell.com>
2024-07-14 12:15:10 -0500

    publish EventModel, minor package updates, logging change

---
+++ src/LatestFilingsEventPublisher/EventModel.cs
@@ -0,0 +1,8 @@
+﻿namespace LatestFilingsEventPublisher;
+
+public class EventModel
+{
+    public string? FormType { get; set; }
+    public string? Summary { get; set; }
+    public string? FilingUrl { get; set; }
+}
